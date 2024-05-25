35581eb383ec655481648e8119c294c89146097c
CodeByBlazej <codebyblazej@gmail.com>
2024-05-25 09:33:48 +0100

    make if statement to get nb from user

--- random.rb
+++ random.rb
@@ -1,11 +1,12 @@
-str = gets.chomp
+number = gets.chomp.to_i
 
-def convert (str)
-  if str.length > 10
-    puts str.upcase
-  else
-    puts str
-  end
+if number >= 0 && number <= 50
+  puts "range 0 - 50"
+elsif number > 50 && number <= 100
+  puts "range 51 - 100"
+elsif number > 100
+  puts "over 100"
+else
+  puts "below 0"
 end
 
-convert (str)

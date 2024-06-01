cb526c75f00bde5545a360109bea68e30550acc6
CodeByBlazej <codebyblazej@gmail.com>
2024-06-01 22:11:45 +0100

    add tripple adder exercise

--- random.rb
+++ random.rb
@@ -1,12 +1,6 @@
-number = gets.chomp.to_i
-
-if number >= 0 && number <= 50
-  puts "range 0 - 50"
-elsif number > 50 && number <= 100
-  puts "range 51 - 100"
-elsif number > 100
-  puts "over 100"
-else
-  puts "below 0"
+def triple_adder(a, b, c)
+  x = a.to_s + b.to_s + c.to_s
+  p "#{x}!"
 end
 
+triple_adder(1, 1, 1)
